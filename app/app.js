05e8ebe8c6c453e92e5e298045038e00fc87dbb3
Michael Hamilton <michaelhamilton626@gmail.com>
2021-01-28 14:39:06 -0500

    wip

--- app/app.js
+++ app/app.js
@@ -1,7 +1,15 @@
 require('aframe');
 
-const light = document.getElementById('light');
+const light1 = document.getElementById('light1');
+const light2 = document.getElementById('light2');
+const light3 = document.getElementById('light3');
+const light4 = document.getElementById('light4');
 
+const getColor = () => `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
 setInterval(() => {
-  light.setAttribute('color', `${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255}`);
-},1000);
+  light1.setAttribute('animation', `property: color; to: ${getColor}; dur: 500; easing: linear;`);
+  light2.setAttribute('animation', `property: color; to: ${getColor}; dur: 500; easing: linear;`);
+  light3.setAttribute('animation', `property: color; to: ${getColor}; dur: 500; easing: linear;`);
+  light4.setAttribute('animation', `property: color; to: ${getColor}; dur: 500; easing: linear;`);
+
+},500);
